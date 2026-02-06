8565663002eb94eab200d143980a3055e5a84626
Lilian <210286008+liliann19@users.noreply.github.com>
2026-02-06 11:37:40 -0800

    implemented removeNode

--- src/Toolbox.java
+++ src/Toolbox.java
@@ -101,7 +101,14 @@ public class Toolbox {
     if (node == null) {
       throw new IllegalArgumentException("Node cannot be null.");
     }
-    
+
+    if (node.prev != null){
+      node.prev.next = node.next;
+    }
+
+    if (node.next != null) {
+      node.next.prev = node.prev;
+    }
   }
 
   /**
